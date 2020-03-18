21d5bf8f5df7b7c0290c84fd88806f878a39b74b
MadsEyv <madsskole97@gmail.com>
2020-03-18 16:43:22 +0100

    Add files via upload

--- INFO134.js
+++ INFO134.js
@@ -1,20 +1,64 @@
-function load(url) {
+
+//vi lagde en universal konstruktør for å gjøre ting mer oversiktlig.
+function constr(file) {
+  this.data = undefined;
+  this.load = function(fiks) {
   let xhr = new XMLHttpRequest();
-  xhr.open("GET", "http://wildboy.uib.no/~tpe056/folk/104857.json");
-  xhr.onreadystatechange = function(){
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      var response = JSON.parse(xhr.responseText);
-      //console.log(response);
-      //console.log(response.elementer.Moss.Kvinner);
+    xhr.open("GET", file);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        category = JSON.parse(xhr.responseText);
+        if (file == "Befolkning") {
+          befolkning.data = category.elementer;
+          befolkning.onload();
+        }
+        if (file == "Sysselsatte") {
+          sysselsatte.data = category.elementer;
+          sysselsatte.onload();
+        }
+        if (file == "Utdanning") {
+          utdanning.data = category.elementer;
+          utdanning.onload();
+        }
+  }
     }
   };
-  xhr.send()
-}
+  this.getNames = function() {
+    // Vi legger til alle elementer i en liste.//
+    var list = []
+    for (var x in this.data) {
+      liste.push(x);
+    }
+    return list;
+  };
 
-function visdata(data, kommune) {
-  return data.elementer[kommune].Menn[tall] + return data.elementer[kommune].Kvinner[tall]
-}
+  this.getIDs = function() {
+      // samme som getNames, men vi spesifiserer kun kommunenummer in i lista.//
+    var list = []
+    for (var x in this.data) {
+      liste.push(this.data[x].kommunenummer);
+    }
+    return list;
+  };
+/* Her tar vi et parameter kommunenr, og iterer over for å sammenligne.
+   Derved tar vi å lager et nytt object(information), og legger til properties
+   som name og data av den valgte kommunen.  */
+  this.getInfo = function(kommunenr) {
+    for (var x in this.data) {
+      if (this.data[x].kommunenummer == kommunenr) {
+        var information = new Object();
+        information.name = x;
+        information.data = this.data[g];
+        return information
+      }
+    }
+    return list;
+  };
+};
 
-function sjekk(antall) {
-  return data.datasett[kommune]
-}
+var befolkning = new constructor("http://wildboy.uib.no/~tpe056/folk/104857.json");
+befolkning.load("Befolkning");
+var sysselsatte = new constructor("http://wildboy.uib.no/~tpe056/folk/100145.json");
+sysselsatte.load("Sysselsatte");
+var utdanning = new constructor("http://wildboy.uib.no/~tpe056/folk/85432.json");
+sysselsatte.load("Utdanning");
