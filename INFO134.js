25dde49f91e66abb90c67d550bc0098c45441cc8
MadsEyv <madsskole97@gmail.com>
2020-04-06 13:30:03 +0200

    Add files via upload

--- INFO134.js
+++ INFO134.js
@@ -1,31 +1,50 @@
 
-//vi lagde en universal konstruktør for å gjøre ting mer oversiktlig.
-function constr(file) {
-  this.data = undefined;
-  this.load = function(fiks) {
-  let xhr = new XMLHttpRequest();
-    xhr.open("GET", file);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        category = JSON.parse(xhr.responseText);
-        if (file == "Befolkning") {
-          befolkning.data = category.elementer;
-          befolkning.onload();
-        }
-        if (file == "Sysselsatte") {
-          sysselsatte.data = category.elementer;
-          sysselsatte.onload();
-        }
-        if (file == "Utdanning") {
-          utdanning.data = category.elementer;
-          utdanning.onload();
-        }
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+};
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+};
+function removediv(div) {
+  var liste = ["intro", "oversikt", "detaljer", "sammenligning"];
+  for (var i in liste) {
+    if (liste[i] == div) {
+      document.getElementById(liste[i]).style.display = "block";
+    }
+    else{
+      document.getElementById(liste[i]).style.display = "none";
+    }
   }
+};
+
+function constructor(fil){
+//vi lagde en universal konstruktør for å gjøre ting mer oversiktlig.
+this.load = function(kategori){
+  //denne funksjonen sender forespørsel om å laste ned datasettet.
+  var respons = undefined;
+  var htp = new XMLHttpRequest();
+  htp.open("GET", fil, true);
+  htp.onreadystatechange = function() {
+    if (htp.readyState == 4 && htp.status == 200){
+      respons = JSON.parse(htp.responseText);
+      //kategori avgjør hvilke objektet som skal få datasettet
+      if (kategori == "befolk"){
+        // datasetter blir så lagt til i data til objektet
+        befolkning.data = respons.elementer;
+      }
+      if (kategori == "syssels") {
+        sysselsatte.data = respons.elementer;
+      }
+      if (kategori == "utdan") {
+        utdanning.data = respons.elementer;
+      }
     }
   };
+  htp.send();
+};
   this.getNames = function() {
     // Vi legger til alle elementer i en liste.//
-    var list = []
+    var list = [];
     for (var x in this.data) {
       liste.push(x);
     }
@@ -42,7 +61,7 @@ function constr(file) {
   };
 /* Her tar vi et parameter kommunenr, og iterer over for å sammenligne.
    Derved tar vi å lager et nytt object(information), og legger til properties
-   som name og data av den valgte kommunen.  */
+   som name og data av den valgte kommunen. */
   this.getInfo = function(kommunenr) {
     for (var x in this.data) {
       if (this.data[x].kommunenummer == kommunenr) {
@@ -57,8 +76,79 @@ function constr(file) {
 };
 
 var befolkning = new constructor("http://wildboy.uib.no/~tpe056/folk/104857.json");
-befolkning.load("Befolkning");
+befolkning.load("befolk");
 var sysselsatte = new constructor("http://wildboy.uib.no/~tpe056/folk/100145.json");
-sysselsatte.load("Sysselsatte");
+sysselsatte.load("syssels");
 var utdanning = new constructor("http://wildboy.uib.no/~tpe056/folk/85432.json");
-sysselsatte.load("Utdanning");
+utdanning.load("utdan");
+function appendrad(tabell, rowid, dataid) {
+  var rad = document.createElement("TR");
+  rad.setAttribute("id", rowid);
+  document.getElementById(tabell).appendChild(rad);
+  headerdata = document.createElement("TD");
+  datanode = document.createTextNode(dataid);
+  headerdata.appendChild(datanode);
+  document.getElementById(rowid).appendChild(headerdata);
+}
+
+
+function addinfotorad(rowid, dataid) {
+  var add_data = document.createElement("TD");
+  var datanode = document.createTextNode(dataid);
+  add_data.appendChild(datanode);
+  document.getElementById(rowid).appendChild(add_data);
+}
+
+function oversiktTabell(){
+  document.getElementById("oversikt").innerHTML="";
+  var tabell1 = document.createElement("TABLE");
+  tabell1.setAttribute("id", "tabell1");
+  document.getElementById('oversikt').appendChild(tabell1);
+  appendrad("tabell1", "byer", "kommuner");
+  appendrad("tabell1", "ider", "kommuneID");
+  appendrad("tabell1", "bef", "befolkning");
+  appendrad("tabell1", "befvekst", "befolkningsvekst");
+
+  for (var i in befolkning.data) {
+    addinfotorad("byer", i);
+    addinfotorad("ider", befolkning.data[i].kommunenummer);
+    var menn = befolkning.data[i].Menn[2018];
+    var kvinner = befolkning.data[i].Kvinner[2018];
+    var test = menn + kvinner;
+    addinfotorad("bef", test);
+    var menngamle = befolkning.data[i].Menn[2007];
+    var kvinngamle = befolkning.data[i].Kvinner[2007];
+    var oldgang = menngamle + kvinngamle;
+    var befolkningsvekst = Math.floor((test - oldgang) / test * 100) + " %"
+    addinfotorad("befvekst", befolkningsvekst);
+  }
+
+};
+function detaljTabell(){
+  document.getElementById("detaljer").innerHTML="";
+  var tabell2 = document.createElement("TABLE");
+  tabell2.setAttribute("id", "tabell2");
+  document.getElementById('detaljer').appendChild(tabell2);
+  var id = document.getElementById("inputnr".value)
+  try {
+  /*
+  var befolkningsinfo = befolkning.getInfo(id)
+  var sysselsatteinfo = sysselsatte.getInfo(id)
+  var utdanningsinfo = utdanning.getInfo(id)
+  */
+  if(id == "") throw "ingenting skrevet!";
+  }
+  catch(err) {
+    document.getElementById('detaljer').innerHTML= "Feil:" + err;
+  }
+    appendrad("tabell2", "byer", "kommuner");
+    appendrad("tabell2", "ider", "kommuneID");
+    appendrad("tabell2", "bef", "befolkning");
+    appendrad("tabell2", "befvekst", "befolkningsvekst");
+    appendrad("tabell2", "statsant", "statistikk for sysselsetting");
+    appendrad("tabell2", "statspros", "prosent for sysselsetting");
+    appendrad("tabell2", "utdanant", "utdanning antall");
+    appendrad("tabell2", "utdanpros", "utdanning prosent");
+
+};
+  var id = document.getElementById("inputnr".value)
